aa76e729d102b56d8d8fba423f69b92469007ddc
sysuwuhaibin <122128609@qq.com>
2023-10-17 14:51:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import numpy as np
 
 with st.chat_message("user"):
     st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
+    st.write("Nice 👋")
 
 message = st.chat_message("assistant")
 message.write("Hello human")
-message.bar_chart(np.random.randn(30, 3))
