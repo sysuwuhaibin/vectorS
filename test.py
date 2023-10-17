f6cf0a6e6ccd71f02fac183ad20e447bdff07438
sysuwuhaibin <122128609@qq.com>
2023-10-17 14:25:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,19 @@
-# import xlrd
-# from milvus_utility import MilvusUtility
-# from openai_utility import ChatBot
-#
-# MilvusUtility().create_collection1('GUIHUAZHIXUN')
-# # 打开Excel文件
-# workbook = xlrd.open_workbook('E:\项目管理\FastGPT问答\规划咨询问题集.xls')
-#
-# # 获取第一个工作表
-# worksheet = workbook.sheet_by_index(0)
-#
-# i = 1
-# # 遍历行
-# for row in range(worksheet.nrows):
-#     # 遍历列
-#     cell_value = worksheet.cell_value(row, 2)
-#     print(cell_value)
-#     data_embedding_res0 = ChatBot().generate_embedding(str(cell_value))
-#     MilvusUtility().insert_entity('GUIHUAZHIXUN', [
-#         {'classification': worksheet.cell_value(row, 0), 'description': worksheet.cell_value(row, 1),
-#          'content': worksheet.cell_value(row, 2),
-#          'embeddings': data_embedding_res0}])
-#     print(i)
-#     i += 1
 import streamlit as st
-import pandas as pd
-
-st.write("""
-# My first app
-## dffdf
-Hello *world!*
-
-- ddfdf
-- dfdfd
-- dfsfd
-""")
-
-df = pd.read_excel("E:\\项目管理\\FastGPT问答\\23.xls")
-st.altair_chart(df)
-
-st.table(df)
+st.button('Hit me')
+st.data_editor('Edit data', data)
+st.checkbox('Check me out')
+st.radio('Pick one:', ['nose','ear'])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
+st.slider('Slide me', min_value=0, max_value=10)
+st.select_slider('Slide to select', options=[1,'2'])
+st.text_input('Enter some text')
+st.number_input('Enter a number')
+st.text_area('Area for textual entry')
+st.date_input('Date input')
+st.time_input('Time entry')
+st.file_uploader('File uploader')
+st.download_button('On the dl', data)
+st.camera_input("一二三,茄子!")
+st.color_picker('Pick a color')
 
