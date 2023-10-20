14af35af1a656f9b4c3d199f3b1f2721f7caa6b8
sysuwuhaibin <122128609@qq.com>
2023-10-20 13:49:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -154,3 +154,4 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder.markdown(full_response, unsafe_allow_html=True)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
+    st.balloons()
